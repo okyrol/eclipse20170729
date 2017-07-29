ef9428852a8b2bfe0699982068a5c7aa1746910e
qimeng <qimeng@163.com>
2017-07-29 15:24:19 +0800

    尝试一下

---
+++ src/qimengDemo/Demo01.java
@@ -0,0 +1,18 @@
+package qimengDemo;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		System.out.println("hello git");
+
+		System.out.println("I am testing you, git!");
+
+		System.out.println("I am testing you, git! second");
+
+		System.out.println("I am testing you, git! thrid");
+
+		System.out.println("I am testing you, git! forth!!!@github");
+		
+	}
+	
+}
